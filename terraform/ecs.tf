aabc5d1312a5c186b99480c65a56cfd7195b43f0
faavour <favourjohn862@gmail.com>
2023-02-02 14:56:55 +0100

    feat:added terraform ecs

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -18,3 +18,11 @@ resource "aws_ecs_cluster" "lifebit-cluster" {
     execute_command_configuration {
       kms_key_id = aws_kms_key.lifebit-app-kms.arn
       logging    = "OVERRIDE"
+
+           log_configuration {
+        cloud_watch_encryption_enabled = true
+        cloud_watch_log_group_name     = aws_cloudwatch_log_group.lifebit-app-log-group.name
+      }
+    }
+  }
+}
