56857d391661af303db8d17f8dcade160f5eb866
faavour <favourjohn862@gmail.com>
2023-02-02 15:14:25 +0100

    feat:added terraform ecs

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -142,3 +142,8 @@ resource "aws_iam_role_policy_attachment" "ecs-iam-policy-attachment" {
   role       = aws_iam_role.lifebit-role.name
   policy_arn = aws_iam_policy.lifebit-policy.arn
 }
+
+resource "aws_iam_role_policy_attachment" "ecs-task-execution-role-policy-attachment" {
+  role       = aws_iam_role.lifebit-role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
