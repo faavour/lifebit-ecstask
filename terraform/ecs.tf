833fd5628d5948de66448b051f5d9dbd3709ba08
faavour <favourjohn862@gmail.com>
2023-02-02 15:20:52 +0100

    feat:added terraform ecs

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -182,3 +182,12 @@ resource "aws_appautoscaling_policy" "ecs_policy_cpu" {
   resource_id        = aws_appautoscaling_target.ecs_target.resource_id
   scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
   service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+   predefined_metric_specification {
+     predefined_metric_type = "ECSServiceAverageCPUUtilization"
+   }
+ 
+   target_value       = 60
+  }
+}
