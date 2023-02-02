bd1eacd26bc3c9ffdc64bd7feef292bfbee137bc
faavour <favourjohn862@gmail.com>
2023-02-02 14:54:59 +0100

    feat:added terraform ecs

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -2,3 +2,6 @@ resource "aws_kms_key" "lifebit-app-kms" {
   description             = "lifebit-app-kms"
   deletion_window_in_days = 7
 }
+resource "aws_cloudwatch_log_group" "lifebit-app-log-group" {
+  name = "lifebit-app-log-group"
+}
