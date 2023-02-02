fa08099c0af177933d57399b83e98cb7501424c1
faavour <favourjohn862@gmail.com>
2023-02-02 14:55:36 +0100

    feat:added terraform ecs

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -2,6 +2,15 @@ resource "aws_kms_key" "lifebit-app-kms" {
   description             = "lifebit-app-kms"
   deletion_window_in_days = 7
 }
+
 resource "aws_cloudwatch_log_group" "lifebit-app-log-group" {
   name = "lifebit-app-log-group"
 }
+
+# This would provision ECS cluster, service and task definition
+resource "aws_ecs_cluster" "lifebit-cluster" {
+  name = "lifebit-cluster"
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
