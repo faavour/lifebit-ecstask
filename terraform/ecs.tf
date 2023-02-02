05b396d75dc6ab70df1664e5a0e74bec28aad922
faavour <favourjohn862@gmail.com>
2023-02-02 15:11:58 +0100

    feat:added terraform ecs

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -105,3 +105,16 @@ resource "aws_iam_policy" "lifebit-policy" {
 
   # Terraform's "jsonencode" function converts a
   # Terraform expression result to valid JSON syntax.
+    policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+      Effect = "Allow",
+      Action = [
+        "elb:*",
+      ],
+      Resource = "*"
+    }
+    ]
+  })
+}
