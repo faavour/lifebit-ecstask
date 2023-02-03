8d43642c62f3ae8d49ef74a0b1ffbe31c12855e6
faavour <favourjohn862@gmail.com>
2023-02-03 17:37:20 +0100

    feat:added variable file

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,6 @@ variable "vpc_cider" {
     description = "(The Cider block to use when setting up the VPC"
     default = "10.0.0.0/16"
 }
+variable "public_subnet_ciders" {
+    type = list(string)
+    
