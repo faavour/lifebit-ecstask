4d77a020c564cfd2ac4212439886fb756b0c19b1
faavour <favourjohn862@gmail.com>
2023-02-03 12:01:56 +0100

    feat:added vpc

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -47,3 +47,13 @@ resource "aws_route_table_association" "rta" {
 resource "aws_security_group" "http_traffic" {
   name        = "allow_http"
   description = "Allow http traffic"
+    vpc_id      = aws_vpc.lifebit-vpc.id
+
+  ingress {
+    description      = "HTTP from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
