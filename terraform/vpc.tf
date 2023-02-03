6428f9e544da7efef2bd93df69eddc80e904f287
faavour <favourjohn862@gmail.com>
2023-02-03 12:02:54 +0100

    feat:added vpc

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -65,3 +65,7 @@ resource "aws_security_group" "http_traffic" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+   egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
