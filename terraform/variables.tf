74a9e10d42ecc1e251abacd1d80210927473e1f0
faavour <favourjohn862@gmail.com>
2023-02-03 17:46:26 +0100

    feat:added variable file

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,13 +3,21 @@ variable "vpc_cider" {
     description = "(The Cider block to use when setting up the VPC"
     default = "10.0.0.0/16"
 }
+
 variable "public_subnet_ciders" {
     type = list(string)
     description = "The subnet ciders for the public subnet"
     default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
+
 variable "aws_azs" {
     type = list(string)
     description = "List of availability zones to deploy resources into"
     default = ["eu-west-2a", "eu-west-2b"]
 }
+
+variable "vpc_id" {
+    type = string
+    description = "The ID of the VPC to deploy resources into"
+}
+
